cadb248c084f450231ad1e5e485406330827afd1
Soumitra <projwebinfo@gmail.com>
2023-05-23 21:49:24 +0530

    Update SpringStoredProcedureSimpleJdbcCallApp.java

--- spring-stored-procedure-simplejdbccall/src/main/java/com/roytuts/spring/stored/procedure/simplejdbccall/SpringStoredProcedureSimpleJdbcCallApp.java
+++ spring-stored-procedure-simplejdbccall/src/main/java/com/roytuts/spring/stored/procedure/simplejdbccall/SpringStoredProcedureSimpleJdbcCallApp.java
@@ -3,7 +3,7 @@ package com.roytuts.spring.stored.procedure.simplejdbccall;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.roytuts.spring.stored.procedure.simplejdbccall")
+@SpringBootApplication
 public class SpringStoredProcedureSimpleJdbcCallApp {
 
 	public static void main(String[] args) {
